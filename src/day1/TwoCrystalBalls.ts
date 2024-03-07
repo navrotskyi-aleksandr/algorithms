c5bd0d412364a13d5d48ab35e7e4eae6c704dfbf
Navrotskyi Aleksandr <navrotskyi.aleksandr@gmail.com>
2024-03-07 16:43:29 +0000

    added two crystalballs

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+  let i = jmpAmount;
+  for (; i < breaks.length; i += jmpAmount) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  i -= jmpAmount;
+
+  for (let j = i; j < i + jmpAmount && j < breaks.length; j++) {
+    if (breaks[j]) {
+      return j;
+    }
+  }
+
+  return -1;
 }
